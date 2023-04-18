98af4ea559108a41954bec3d4ce9c8215e06e938
Julio Cesar <juliojc12@gmail.com>
2023-04-18 09:28:58 -0300

    Create swagger documentation, and restore deletemethod

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -29,6 +29,7 @@ public class MovieController : ControllerBase
     /// <param name="movieDto">Objeto com os campos necessários para criação de um filme.</param>
     /// <returns>IActionResult</returns>
     /// <response code="201">Retorna o objeto criado</response>
+    /// <response code="400">Retorna uma mensagem de erro</response>
     [HttpPost]
     public IActionResult AddMovie([FromBody] CreateMovieDto movieDto)
     {
@@ -38,6 +39,14 @@ public class MovieController : ControllerBase
         return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
     }
 
+    ///<sumary>
+    /// Retorna uma lista de filmes
+    /// </sumary>
+    /// <param name="skip">Quantidade de filmes a serem pulados</param>
+    /// <param name="take">Quantidade de filmes a serem retornados</param>
+    /// <returns>Lista de filmes</returns>
+    /// <response code="200">Retorna uma lista de filmes</response>
+    /// <response code="404">Retorna uma mensagem de erro</response>
     [HttpGet]
     public IEnumerable<Movie> GetMovies([FromQuery] int skip=0,
                                         [FromQuery] int take=20)
@@ -45,6 +54,14 @@ public class MovieController : ControllerBase
         return _context.Movies.Skip(skip).Take(take);
     }
 
+
+    /// <summary>
+    /// Retorna um filme pelo seu Id
+    /// </summary>
+    /// <param name="id">Id do filme</param>
+    /// <returns>Retorna o filme de acordo com o id informado</returns>
+    /// <response code="200">Retorna o filme de acordo com o id informado</response>
+    /// <response code="404">Retorna uma mensagem de erro</response>
     [HttpGet("{id}")]
     public IActionResult GetMovieById(Guid id)
     {
@@ -56,6 +73,15 @@ public class MovieController : ControllerBase
         return Ok(movie);
     }
 
+
+    /// <summary>
+    /// Atualiza um filme
+    /// </summary>
+    /// <param name="id">Id do filme</param>
+    /// <param name="movieDto">Objeto com os campos necessários para atualização de um filme.</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Retorna uma mensagem de sucesso</response>
+    /// <response code="404">Retorna uma mensagem de erro</response>
     [HttpPut("{id}")]
     public IActionResult UpdateMovie(Guid id, [FromBody] UpdateMovieDto movieDto)
     {
@@ -70,7 +96,14 @@ public class MovieController : ControllerBase
         return NoContent();
     }
 
-
+    /// <summary>
+    /// Atualiza parcialmente um ou mais campos de um filme
+    /// </summary>
+    /// <param name="id">Id do Filme</param>
+    /// <param name="patchDoc">Campos a serem atualizado </param>
+    /// <returns>IActionsResult</returns>
+    /// <response code="204">Retorna uma mensagem de sucesso</response>
+    /// <response code="404">Retorna uma mensagem de erro</response>
     [HttpPatch("{id}")]
     public IActionResult PartiallyUpdateMovie(Guid id, [FromBody] JsonPatchDocument<UpdateMovieDto> patchDoc)
     {
@@ -89,5 +122,30 @@ public class MovieController : ControllerBase
         _context.SaveChanges();
 
         return NoContent();
+    }
+
+
+    /// <summary>
+    /// Exclui um filme pelo id
+    /// </summary>
+    /// <param name="id">Id do filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Retorna uma mensagem de sucesso</response>
+    /// <response code="404">Retorna uma mensagem de erro</response>
+    [HttpDelete("{id}")]
+    public IActionResult DeleteMovie(Guid id)
+    {
+        var movieFromDb = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+        if (movieFromDb == null)
+        {
+            return NotFound();
+        }
+
+        _context.Remove(movieFromDb);
+        _context.SaveChanges();
+
+        return NoContent();
+
+
     }
 }
